349c97fae7e4a2be2ec34351965e0358e2aa4180
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-03-14 13:20:45 +0300

    ee

--- Судьба.py
+++ Судьба.py
@@ -20,14 +20,31 @@ def Fight(fighter_1, fighter_2):
             a.health -= int(b.strike())
             if a.health <= 0:
                 print(f"{b.name} побеждает!")
-              
-class Servant:
     
-    def __init__(self, name, health, attack, strength):
+            
+
+class Servant:
+    def __init__(self, name, strength, vitality, agility, mana, luck):
+        self.name = name
+        self.strength = strength
+        self.vitality = vitality
+        self.agility = agility
+        self.mana = mana
+        self.luck = luck
+        self.stats_list = [self.strength, self.vitality, self.agility, self.mana, self.luck]
+    
+    def convert_to_stats(self):
+        i = 0
+        while i < 4:
+            print(self.stats_list[i])
+            i+=1
+
+
+    """def __init__(self, name, health, attack, strength):
         self.name = name
         self.health = health
         self.attack = attack
-        self.strength = strength
+        self.strength = strength"""
 
     def introduce(self):
         print(f"Меня зовут {self.name}")
@@ -35,7 +52,9 @@ class Servant:
     def strike(self):
         strike_damage = ran.randint(1, self.attack) + self.strength
         return strike_damage
-     
-Spartak = Servant("Спартак", 30, 10, 4)
-Arthur = Servant("Король Артур", 40, 8, 3)
-Fight(Spartak, Arthur)
+   
+   
+Spartak = Servant("Спартак", 12, 100, 10, 5, 7)
+print(Spartak.convert_to_stats())
+"""Arthur = Servant("Король Артур", 40, 8, 3)
+Fight(Spartak, Arthur)"""
